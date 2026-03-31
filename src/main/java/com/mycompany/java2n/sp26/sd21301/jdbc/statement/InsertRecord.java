b3df16c39ed89702752eb2f90a1a89e8ff67cd5f
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-03-31 17:37:41 +0700

    JDBC: Insert record into table

---
+++ src/main/java/com/mycompany/java2n/sp26/sd21301/jdbc/statement/InsertRecord.java
@@ -0,0 +1,63 @@
+package com.mycompany.java2n.sp26.sd21301.jdbc.statement;
+
+import com.mycompany.java2n.sp26.sd21301.jdbc.DatabaseConnectionManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class InsertRecord {
+
+    public static void main(String[] args) {
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("java2N_sp26_sd21301", "sa", "123456");
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+
+            connection = dcm.getConnection();
+
+            statement = connection.createStatement();
+
+            String sql = """
+                            INSERT INTO students(Id, Name, Age)
+                            VALUES (3, 'Student 3', 23);
+                         """;
+
+            statement.executeUpdate(sql);
+
+            System.out.println("Done...");
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        } finally {
+
+            // close
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        }
+
+    }
+
+}

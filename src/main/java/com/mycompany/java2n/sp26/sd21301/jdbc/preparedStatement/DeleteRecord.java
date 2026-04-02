472c63c7e0dc8495412d0b1b761fdc943fb53c12
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-04-02 17:13:50 +0700

    Delete using preparedStatement

---
+++ src/main/java/com/mycompany/java2n/sp26/sd21301/jdbc/preparedStatement/DeleteRecord.java
@@ -0,0 +1,67 @@
+package com.mycompany.java2n.sp26.sd21301.jdbc.preparedStatement;
+
+import com.mycompany.java2n.sp26.sd21301.ass1.Student;
+import com.mycompany.java2n.sp26.sd21301.jdbc.DatabaseConnectionManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DeleteRecord {
+
+    public static void main(String[] args) {
+
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("java2N_sp26_sd21301", "sa", "123456");
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+
+            connection = dcm.getConnection();
+
+            String sql = """
+                         DELETE FROM students
+                         WHERE Id=?;
+                         """;
+
+            statement = connection.prepareStatement(sql);
+
+
+            statement.setInt(1, 3);
+
+            statement.executeUpdate();
+
+            System.out.println("Done...");
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        } finally {
+
+            // close
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+}

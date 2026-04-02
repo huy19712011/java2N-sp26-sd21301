97a979aad3b2303adfb543bc2f3d32dec5c53747
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-04-02 17:47:43 +0700

    Select using prepared statement

---
+++ src/main/java/com/mycompany/java2n/sp26/sd21301/jdbc/preparedStatement/SelectV2.java
@@ -0,0 +1,77 @@
+package com.mycompany.java2n.sp26.sd21301.jdbc.preparedStatement;
+
+import com.mycompany.java2n.sp26.sd21301.ass1.Student;
+import com.mycompany.java2n.sp26.sd21301.jdbc.DatabaseConnectionManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class SelectV2 {
+
+    public static void main(String[] args) {
+
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("java2N_sp26_sd21301", "sa", "123456");
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+
+            connection = dcm.getConnection();
+
+            String sql = """
+                         SELECT Id, Name, Age FROM students
+                         WHERE Id=?;
+                         """;
+
+            statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, 4);
+
+            resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+
+                int id = resultSet.getInt("Id");
+                String name = resultSet.getString("Name");
+                int age = resultSet.getInt("Age");
+
+                Student student = new Student(id, name, age);
+
+                System.out.println(student);
+            }
+
+            System.out.println("Done...");
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        } finally {
+
+            // close
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+}
